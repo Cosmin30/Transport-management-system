1f376062a13b02f68de4d990a30d5e034d949f78
Cocea Cosmin <cocea2003@gmail.com>
2025-04-13 18:50:50 +0300

    updates

--- Aplicatie Transporturi/frontend/src/app/app.routes.ts
+++ Aplicatie Transporturi/frontend/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home/home.component';
 import { VehicleListComponent } from './modules/vehicles/vehicle-list/vehicle-list.component';
+import { DriverListComponent } from './modules/drivers/driver-list/driver-list.component';
+import { DeliveryListComponent } from './modules/deliveries/delivery-list/delivery-list.component';
 import { LoginRegisterComponent } from './modules/auth/login-register/login-register.component';
 
 export const routes: Routes = [
-  { path: '', component: VehicleListComponent },
+  { path: '', component: HomeComponent },
+  { path: 'vehicles', component: VehicleListComponent },
+  { path: 'drivers', component: DriverListComponent },
+  { path: 'deliveries', component: DeliveryListComponent },
   { path: 'auth', component: LoginRegisterComponent }
 ];
