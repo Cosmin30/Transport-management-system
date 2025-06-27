b45dbc401e88d784829e7dda5e47619c9156199b
Cosmin30 <cocea2003@gmail.com>
2025-06-27 20:49:56 +0300

    changes

--- Aplicatie Transporturi/Extensions/IdentityServiceExtensions.cs
+++ Aplicatie Transporturi/Extensions/IdentityServiceExtensions.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Tokens; 
 using System.Text;
 
 namespace Aplicatie_Transporturi.Extensions
